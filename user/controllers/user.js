f9246e1b6828d349412ce6a15d10a9a1a722204b
harish sambasivam <imharishsambasivam@gmail.com>
2021-08-13 07:54:37 +0530

    added create and get users changes for new schema

--- user/controllers/user.js
+++ user/controllers/user.js
@@ -4,7 +4,7 @@ const getUser = async (userId) => {
   let result,
     err = null;
   try {
-    const response = await db.query("SELECT * FROM users");
+    const response = await db.query('SELECT * FROM public."Users"');
     result = response.rows;
   } catch (e) {
     err = {
@@ -15,15 +15,55 @@ const getUser = async (userId) => {
   return { result, err };
 };
 
+const validateUserData = (userData) => {
+  const date = Date.now();
+  userData["createdAt"] = userData?.createdAt ? userData.createdAt : date;
+  userData["updatedAt"] = date;
+  return userData;
+};
+
 const addUser = async (userData) => {
   let result,
     err = null;
   const client = await db.getClient();
   try {
-    const { email, hasFarm, location, password, status, username } = userData;
+    console.log(userData);
+    const {
+      firstName,
+      lastName,
+      userName,
+      bio,
+      avatar,
+      dob,
+      website,
+      location,
+      email,
+      phone,
+      category,
+      gender,
+      verifiedUser,
+      createdAt,
+      updatedAt,
+    } = validateUserData(userData);
     const response = await client.query(
-      "INSERT INTO users (email, hasFarm, location,password, status,username ) VALUES ($1,$2,$3,$4,$5,$6)",
-      [email, hasFarm, location, password, status, username]
+      'INSERT INTO public."Users" (firstName,lastName,userName, bio,avatar, dob,website, location, email,phone, category,gender, verifiedUser,createdAt, updatedAt) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)',
+      [
+        firstName,
+        lastName,
+        userName,
+        bio,
+        avatar,
+        dob,
+        website,
+        location,
+        email,
+        phone,
+        category,
+        gender,
+        verifiedUser,
+        createdAt,
+        updatedAt,
+      ]
     );
     if (response.rowCount === 1) {
       result = "success";
